42136e6ec77eb51e773556feeba10449b9075ef9
zhutya <zhutya@yonyou.com>
2020-03-05 10:56:46 +0800

    引入登录组件

--- my-app2/README.md
+++ my-app2/README.md
@@ -1,6 +1,9 @@
 ## 1.在 create-react-app 中使用antd  
-按需打包，优化打包大小：https://ant.design/docs/react/use-with-create-react-app-cn  
-自定义主题：https://ant.design/docs/react/customize-theme-cn
+按需打包，优化打包大小官网步骤：https://ant.design/docs/react/use-with-create-react-app-cn  
+自定义主题官网教程：https://ant.design/docs/react/customize-theme-cn
 ## 2.引入路由  
 npm i react-router-dom  
-一级路由：登录页面 login.js 和主界面 admin.js
+页面设计思路 一级路由：登录页面 login.js 和主界面 admin.js（非home界面）
+## 3.重置样式，登录页面  
+图片在页面上的的引入：import xxx from xxx <img src={xxx}/>  
+引入antd的Form组件官方教程：https://ant.design/components/form-cn/
--- my-app2/src/pages/login/login.js
+++ my-app2/src/pages/login/login.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react'
+import './login.less'
+import logo from './images/logo.png'
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 // 登陆的路由组件
 export default class Login extends Component {
+    handleSubmit = (event) => {
+
+    }
     render() {
         return (
-            <div>
-                Login组件
+            <div className="login">
+                <div className="login-header">
+                    <img src={logo} alt=''/>
+                    <h1>后台管理系统</h1>
+                </div>
+                <div className="login-content">
+                    <h2>用户登录</h2>
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        onSubmit={this.handleSubmit}
+                        initialValues={{
+                            remember: true,
+                        }}>
+                        <Form.Item
+                            name="username"
+                            rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Username!',
+                            },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                            ]}
+                        >
+                            <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                            />
+                        </Form.Item>
+        
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
         )
     }
