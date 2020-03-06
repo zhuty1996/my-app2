405421294aac8f84f90159b6f922804106f075f2
zhutya <zhutya@yonyou.com>
2020-03-06 15:37:36 +0800

    导航栏配置跳转路由

--- my-app2/src/components/left-nav/left-nav.js
+++ my-app2/src/components/left-nav/left-nav.js
@@ -4,12 +4,7 @@ import logo from '../../assets/images/logo.png'
 import {Link} from 'react-router-dom'
 import { Menu } from 'antd';
 import {
-  AppstoreOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
   PieChartOutlined,
-  DesktopOutlined,
-  InboxOutlined,
   MailOutlined,
 } from '@ant-design/icons';
 
@@ -43,8 +38,10 @@ export default class LeftNav extends Component {
                     inlineCollapsed={this.state.collapsed}
                     >
                     <Menu.Item key="1">
-                        <PieChartOutlined />
-                        <span>首页</span>
+                        <Link to='/home'>
+                            <PieChartOutlined />
+                            <span>首页</span>
+                        </Link>
                     </Menu.Item>
 
                     <SubMenu
@@ -57,13 +54,62 @@ export default class LeftNav extends Component {
                         }
                     >
                         <Menu.Item key="2">
-                            <MailOutlined />
-                            <span>品类管理</span>
+                            <Link to='/category'>
+                                <MailOutlined />
+                                <span>品类管理</span>
+                            </Link>
                         </Menu.Item>
 
                         <Menu.Item key="3">
+                            <Link to='/product'>
+                                <MailOutlined />
+                                <span>商品管理</span>
+                            </Link>
+                        </Menu.Item>
+                    </SubMenu>
+
+                    <Menu.Item key="4">
+                        <Link to='/user'>
+                            <PieChartOutlined />
+                            <span>用户管理</span>
+                        </Link>
+                    </Menu.Item>
+
+                    <Menu.Item key="5">
+                        <Link to='/role'>
+                            <PieChartOutlined />
+                            <span>角色管理</span>
+                        </Link>
+                    </Menu.Item>
+
+                    <SubMenu
+                        key="sub2"
+                        title={
+                        <span>
                             <MailOutlined />
-                            <span>商品管理</span>
+                            <span>图形图表</span>
+                        </span>
+                        }
+                    >
+                        <Menu.Item key="6">
+                            <Link to='/chars/bar'>
+                                <MailOutlined />
+                                <span>柱形图</span>
+                            </Link>
+                        </Menu.Item>
+
+                        <Menu.Item key="7">
+                            <Link to='/chars/line'>
+                                <MailOutlined />
+                                <span>折线图</span>
+                            </Link>
+                        </Menu.Item>
+
+                        <Menu.Item key="8">
+                            <Link to='/chars/pie'>
+                                <MailOutlined />
+                                <span>饼状图</span>
+                            </Link>
                         </Menu.Item>
                     </SubMenu>
                 </Menu>
--- my-app2/src/pages/admin/admin.js
+++ my-app2/src/pages/admin/admin.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import {Layout} from 'antd'
+import { Switch, Redirect,Route } from "react-router-dom";
+
 import LeftNav from '../../components/left-nav/left-nav'
 import Header from '../../components/header/header'
+//引入子组件
+import Home from '../home/home'
+import Category from '../category/category'
+import Product from '../product/product'
+import Role from '../role/role'
+import User from '../user/user'
+import Bar from '../charts/bar'
+import Line from '../charts/line'
+import Pie from '../charts/pie'
 
 const { Content, Footer, Sider } = Layout;
 
@@ -15,7 +26,19 @@ export default class Admin extends Component {
                 </Sider>
                 <Layout>
                     <Header>Header</Header>
-                    <Content style={{backgroundColor: 'white'}}>Content</Content>
+                    <Content style={{backgroundColor: 'white'}}>
+                        <Switch>
+                            <Route path='/home' component={Home}/>
+                            <Route path='/category' component={Category}/>
+                            <Route path='/product' component={Product}/>
+                            <Route path='/role' component={Role}/>
+                            <Route path='/user' component={User}/>
+                            <Route path='/chars/bar' component={Bar}/>
+                            <Route path='/chars/line' component={Line}/>
+                            <Route path='/chars/pie' component={Pie}/>
+                            <Redirect to='/home'/>
+                        </Switch>
+                    </Content>
                     <Footer style={{textAlign: 'center', color: '#ccc'}}>推荐使用谷歌浏览器</Footer>
                 </Layout>
             </Layout>
